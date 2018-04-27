09549a7be30588311263644ca72ee57c5d0ad54e
Jim Conner <c_rennochj@outlook.com>
2018-04-27 11:57:25 -0500

    Add click to call and site login

---
+++ rds_config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#config file containing credentials for rds mysql instance
+
+db_username = "conner"
+db_password = "(2Terpins)"
+db_name = "jimcodb"
+db_endpoint = "jimcodb.c6kunvjrwqvj.us-east-1.rds.amazonaws.com"
+db_port = 3306
