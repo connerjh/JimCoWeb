dc86e21a2bd1c897175468c670b829ac3af91bc3
Jim Conner <c_rennochj@outlook.com>
2018-04-26 19:42:46 -0500

    Initial

--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,7 @@
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
-
+from django.template import Context
+from . import jimcodb
 
 def index(request):
 
@@ -8,12 +9,28 @@ def index(request):
 
     if request.user.is_authenticated:
 
+        print('User Id {}: '.format(request.user.username))
+
+        user = jimcodb.get_individual_by_user(request.user.username)
+
+        print('User {}: '.format(user['UserName']))
+
+        context = {
+            'UserId': user['UserName'],
+            'FullName': user['FirstName'] + " " + user['LastName'],
+        }
         template = loader.get_template('portal/index.html')
-        return HttpResponse(template.render({}, request))
+        return HttpResponse(template.render(context, request))
 
     else:
 
         return HttpResponseRedirect('/login/')
+
+def ccp(request):
+
+        template = loader.get_template('portals/ccp.html')
+        return HttpResponse(template.render({}, request))
+
 
 
 
