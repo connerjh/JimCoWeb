f34e173c6b4b11dfbdd05788739836a2daf0644d
Jim Conner <c_rennochj@outlook.com>
2018-04-29 13:32:21 -0500

    commiting

--- portal/urls.py
+++ portal/urls.py
@@ -4,6 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('call', views.call, name="call")
+    path('', views.index, name="index")
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,8 +1,8 @@
 from django.template import loader
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import redirect
-from django.template import Context
 from . import jimcodb
+
 
 def index(request):
 
@@ -22,28 +22,6 @@ def index(request):
             'User': user
         }
         template = loader.get_template('portal/index.html')
-        return HttpResponse(template.render(context, request))
-
-    else:
-
-        return redirect('/login/?next={}'.format(request.path))
-
-
-def call(request):
-    if request.user.is_authenticated:
-
-        print('User Id {}: '.format(request.user.username))
-
-        user = jimcodb.get_individual_by_user(request.user.username)
-
-        print('User {}: '.format(user['UserName']))
-
-        context = {
-            'UserId': user['UserName'],
-            'FullName': user['FirstName'] + " " + user['LastName'],
-            'User': user
-        }
-        template = loader.get_template('portal/call.html')
         return HttpResponse(template.render(context, request))
 
     else:
